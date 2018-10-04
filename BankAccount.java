c35bb83389e64a83c71a2c21686da8ccb7b66427
Sublime <other@Alberts-MBP.fios-router.home>
2018-10-03 22:31:56 -0400

    made bankaccount code

--- BankAccount.java
+++ BankAccount.java
@@ -1,1 +1,36 @@
-
+public class BankAccount {
+  public double balance;
+  public int accountID;
+  public String password;
+  public BankAccount(int x, String s,double b){
+    b = balance;
+    x = accountID;
+    s = password;
+  }
+  public int getaccountID(){
+    return accountID;
+  }
+  public double getbalance(){
+    return balance;
+  }
+  public String setpassword(){
+    return password;
+  }
+  public String toString(){
+    return accountID + "\t" + balance;
+  }
+  public boolean increasebal(int depo){
+    if (depo >= 0){
+      balance += depo;
+      return true;
+    }
+    return false;
+  }
+  public boolean decreasebal(int withdraw){
+    if (withdraw < balance){
+      balance -= withdraw;
+      return true;
+    }
+    return false;
+  }
+}
